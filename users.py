9137043ef69c0f7dc1e6f3c3b85a457b8374f984
Angad2005 <angadsthind@gmail.com>
2025-04-05 19:51:27 +0530

    Cleaning main

--- users.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-conn = sqlite3.connect('users.db')
-cursor = conn.cursor()
-cursor.execute("SELECT username, password FROM users")
-users = cursor.fetchall()
-print("\nRegistered Users:")
-for user in users:
-    print(f"Username: {user[0]}, Password: {user[1]}")
-conn.close()
